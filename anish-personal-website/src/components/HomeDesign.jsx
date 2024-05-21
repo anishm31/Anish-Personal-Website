774a55950505a6e04a6227258e23ffd9d797bff4
Anish Madgula <animadgula@utexas.edu>
2024-05-20 19:30:08 -0500

    added animations and links to my various profiles

--- anish-personal-website/src/components/HomeDesign.jsx
+++ anish-personal-website/src/components/HomeDesign.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import headshot from '../assets/anish-headshot.png'
 import { TypeAnimation } from "react-type-animation"
+import { FaGithub, FaLinkedin, FaGitlab, FaEnvelope } from 'react-icons/fa'
 
 const HomeDesign = () => {
   return (
@@ -19,21 +20,48 @@ const HomeDesign = () => {
                         I'm a
                     </span> <br />
                     <TypeAnimation 
-                    sequence={[
-                        "Full Stack Developer",
-                        1000,
-                        "Engineer",
-                        1000,
-                        "Coding Enthusiast",
-                        1000
-                    ]}
+                        sequence={[
+                            "Full Stack Developer",
+                            1000,
+                            "Engineer",
+                            1000,
+                            "Problem Solver",
+                            1000,
+                            "Innovator",
+                            1000
+                        ]}
                     wrapper="span"
                     speed={50}
                     repeat={Infinity}
                     />
                 </h1>
+                <p className="text-white sm:text-lg my-6 lg:text-xl">
+                    My name is Anish Madgula, and I am a Computer Science student at UT Austin!
+                </p>
 
-                
+                <div className='my-8 flex items-center'>
+                    <a href="/" className='px-6 py-3 w-full sm:w-auto sm:px-4 sm:py-2 text-center rounded-xl mb-2 sm:mb-0 sm:mr-4 bg-gradient-to-br from-blue-500 to-green-600 text-white'>
+                        View Resume
+                    </a>
+                    <a href="/" className='px-6 py-3 w-full sm:w-auto sm:px-4 sm:py-2 text-center rounded-xl mb-2 sm:mb-0 sm:mr-4 border border-gray-400 hover:bg-gradient-to-br from-blue-500 to-green-600 text-white hover:border-none'>
+                        Contact Me
+                    </a>
+
+                    <div className='flex items-center'>
+                        <a href="https://github.com/anishm31" className='text-white mx-2' target="_blank" rel="noopener noreferrer">
+                            <FaGithub size={30} />
+                        </a>
+                        <a href="https://www.linkedin.com/in/anish-madgula/" className='text-white mx-2' target="_blank" rel="noopener noreferrer">
+                            <FaLinkedin size={30} />
+                        </a>
+                        <a href="https://gitlab.com/animadgula" className='text-white mx-2' target="_blank" rel="noopener noreferrer">
+                            <FaGitlab size={30} />
+                        </a>
+                        <a href="mailto:animadgula@gmail.com" className='text-white mx-2'>
+                            <FaEnvelope size={30} />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
